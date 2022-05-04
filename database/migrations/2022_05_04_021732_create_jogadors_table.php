2ded5f52d95bec7d22276fe219a437f9f7fdc32a
Gustavo Almeida <gustavogalvesalmeida@gmail.com>
2022-05-03 23:53:48 -0300

    Models e migrations criados

---
+++ database/migrations/2022_05_04_021732_create_jogadors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Clube;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jogador', function (Blueprint $table) {
+            $table->id();
+            $table->string("nome", 100);
+            $table->string("data", 10);
+            $table->foreignIdFor(Clube::class); // FK que linka no clube
+            $table->foreign("clube_id")->references("id")->on("clube");
+            $table->string("posicao", 100);         
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jogador');
+    }
+};
